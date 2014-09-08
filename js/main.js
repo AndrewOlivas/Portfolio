8a5a3dd82ec03eeacdee63411fcf874c5148991a
Andrew Olivas <andrewrolivas@gmail.com>
2014-09-08 13:29:00 -0700

    social tab

--- js/main.js
+++ js/main.js
@@ -149,3 +149,19 @@ $(document).ready(function(){
 })(jQuery);
 
 // end of js carousel
+
+(function() {
+
+    $('.share').on('click', function(e) {
+
+        e.preventDefault();
+
+        var $this = $(this);
+
+        $this.animate({
+            'width': $this.width() == 304 ? '64px' : '304px'
+        }, 300, 'swing');
+
+    });
+
+} () );
